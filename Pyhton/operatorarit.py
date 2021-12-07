21ab455db37b576268c5c4a5657d00b1282ff373
HafidAlAzhar <hafidgamers11@gmail.com>
2021-12-07 20:13:23 +0700

    Push Hello World, variable, Operator

---
+++ Pyhton/operatorarit.py
@@ -0,0 +1,43 @@
+# Operator Aritmatika
+
+# Penjumlahan
+apel = 12
+jeruk = 24
+jumlah = apel + jeruk
+print(jumlah, "Buah")
+
+# Pengurangan
+hutang = 5000
+bayar = 2000
+sisaHutang = hutang - bayar
+print(sisaHutang, "Ribu")
+
+# Perkalian
+panjang = 30
+lebar = 15
+luas = panjang * lebar
+print(luas, "CM")
+
+# Pembagian
+kue = 40
+anak = 8
+kueperanak = kue / anak
+print( "Setiap anak mendapatakan kue sebanyak : ",kueperanak)
+
+# Sisa bagi / Modulus
+bil1 = 14
+bil2 = 5
+hasil = bil1 % bil2
+print("Sisa bagi dari", bil1, "dan", bil2, "hasilnya", hasil) 
+
+# Pangkat
+bil3 = 8
+bil4 = 4
+hasilPangkat = bil3 ** bil4
+print(hasilPangkat) 
+
+# Pembagian Bulat
+print(10//3)
+
+# Pembagian Tidak Bulat
+print(10/3)
---
+++ Pyhton/variable.py
@@ -0,0 +1,23 @@
+
+
+umur = 15
+print(umur)
+# (type) untuk mengecek tipe data
+type(umur)
+umur = "lima belas"
+print(umur)
+type(umur)
+
+namaDepan = 'Hafid'
+namaBelakang = 'Al Azhar'
+nama = namaDepan + ' ' + namaBelakang
+umur = 15
+hobi = "Badminton"
+print("Biodata\n", nama, "\n", umur, "\n", hobi)
+
+#Menghitung Luas Persegi Panjang
+print("Menghitung Luas Persegi\n")
+panjang = 20
+lebar = 15
+luas = panjang * lebar
+print("Luas Persegi Itu Adalah", luas)
