c4b4cda64bbf8baac595919f61fae48b17a2168a
HafidAlAzhar <hafidgamers11@gmail.com>
2021-12-12 09:09:57 +0700

    Penjumlahan

---
+++ Pyhton/gamepenjumlahan.py
@@ -0,0 +1,13 @@
+# Game Penjumlahan
+
+a = int(input("Silahkan masukan angka 1 : "))
+b = int(input("Silahkan masukan angka 2 : "))
+
+hasil = (a + b)
+
+jwb = int(input(f"Kalikan Angka Angka Berikut {a} + {b} : "))
+
+if jwb == hasil:
+    print("Selamat jawaban anda benar")
+else:
+    print(f"Jawaban Anda Salah, Jawaban yang benar adalah: {hasil}")
