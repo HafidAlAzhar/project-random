0c901394df897eb1e065c432ddb83e4c1a937f80
HafidAlAzhar <hafidgamers11@gmail.com>
2022-02-05 20:10:44 +0700

    Mod PFL

--- Pyhton/program_for_loop.py
+++ Pyhton/program_for_loop.py
@@ -6,7 +6,7 @@ umur = []
 for i in range(0, banyak):
     print(f"Data ke {i}")
     input_nama = input("Masukan nama : ")
-    input_umur = int(input("Masukan Umur : "))k
+    input_umur = int(input("Masukan Umur : "))
 
     nama.append(input_nama)
     umur.append(input_umur)
